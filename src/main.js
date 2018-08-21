4ea7bc81b7f6eb677ce2be3bb48be3080ca00f1d
Dung Truong <checkraiser11@gmail.com>
2018-08-22 06:06:31 +0700

    Fix apollo

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-// @flow
 import 'isomorphic-fetch';
+
 import App, {ApolloClientToken} from 'fusion-apollo';
-import GetApolloClient, { 
-  ApolloClientEndpointToken,
-  ApolloClientAuthKeyToken
-} from 'fusion-apollo-universal-client';
-import UniversalEvents, {UniversalEventsToken} from 'fusion-plugin-universal-events';
 import {FetchToken} from 'fusion-tokens';
-import unfetch from 'unfetch';
-import root from './root';
-import config from '../config/config';
 import Router from 'fusion-plugin-react-router';
 import Styletron from 'fusion-plugin-styletron-react';
+import UniversalEvents, {
+  UniversalEventsToken,
+} from 'fusion-plugin-universal-events';
+
+import ApolloClientPlugin, {
+  ApolloClientEndpointToken,
+} from 'fusion-apollo-universal-client';
+import unfetch from 'unfetch';
+
+import root from './root.js';
+
+import config from '../config/config';
 
 export default () => {
   const app = new App(root);
-  app.register(Styletron);
   __BROWSER__ && app.register(FetchToken, window.fetch);
   __NODE__ && app.register(FetchToken, unfetch);
   app.register(Router);
-
-  app.register(ApolloClientToken, GetApolloClient);
-  app.register(ApolloClientEndpointToken, config.graphQLEndpoint);
-  app.register(ApolloClientAuthKeyToken, "bearer");
+  app.register(Styletron);
   app.register(UniversalEventsToken, UniversalEvents);
+  app.register(ApolloClientToken, ApolloClientPlugin);
+  app.register(ApolloClientEndpointToken, config.graphQLEndpoint);
   return app;
 };
