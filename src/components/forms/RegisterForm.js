c67585252c7981bef7c658b3694efe6604b5ecbf
Dung Truong <checkraiser11@gmail.com>
2018-08-28 06:14:58 +0700

    Refactor form

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -6,23 +6,34 @@ import { Subscribe } from 'unstated';
 
 const FormItem = Form.Item;
 
-const NormalRegisterForm = ({ form: { getFieldDecorator, validateFields } }) => {
+const NormalRegisterForm = ({ form: { getFieldDecorator, validateFieldsAndScroll, getFieldValue } }) => {
   return (
     <Subscribe to={[RegisterContainer]}>
       {container => (
-        <Form onSubmit={container.handleSubmit(validateFields)} className="register-form">
+        <Form onSubmit={container.handleSubmit(validateFieldsAndScroll)} className="register-form">
           <FormItem>
             {getFieldDecorator('email-register', {
               rules: [{ required: true, message: 'Please input your Email!' }],
             })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              <Input prefix={<Icon type="user" style={container.state.emailStyle} />} placeholder="Username" />
             )}
           </FormItem>
           <FormItem>
             {getFieldDecorator('register-password', {
               rules: [{ required: true, message: 'Please input your Password!' }],
             })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              <Input prefix={<Icon type="lock" style={container.state.passwordStyle} />} type="password" placeholder="Password" />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('register-password-confirm', {
+              rules: [{
+                required: true, message: 'Please confirm your password!',
+              }, {
+                validator: container.compareToFirstPassword(getFieldValue('register-password')),
+              }],
+            })(
+              <Input type="password" prefix={<Icon type="lock" style={container.state.passwordStyle} />} placeholder="Confirm password" />
             )}
           </FormItem>
           <FormItem>
--- src/containers/forms/Register.js
+++ src/containers/forms/Register.js
@@ -1,12 +1,28 @@
 import { Container } from 'unstated';
+
+const color = 'rgba(0,0,0,.25)'
+
 class RegisterContainer extends Container {
-  handleSubmit = (validateFields) => (e) => {
+  state = {
+    emailStyle: { color },
+    passwordStyle: { color }
+  }
+
+  handleSubmit = (validateFieldsAndScroll) => (e) => {
     e.preventDefault();
-    validateFields((err, values) => {
+    validateFieldsAndScroll((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
       }
     });
+  }
+
+  compareToFirstPassword = (passwordValue) => (rule, value, callback) => {
+    if (value && value !== passwordValue) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
   }
 }
 
